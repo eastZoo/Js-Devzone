95ce79b9195bbabdb79a1371bc83fed3875b6c7b
DESKTOP-U8HSDMJ\dongj <dongju010@naver.com>
2022-03-04 01:07:23 +0900

    spring server delete

--- eastzoo/src/main/java/spring/eastzoo/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package spring.eastzoo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends CrudRepository<Todo, Long>{
-
-}
